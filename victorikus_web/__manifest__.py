f7c516bdebfca13d222c53d28c2768a2ff018d18
Vladislav Bochkov <vlad.bochkov.ch@gmail.com>
2023-12-06 21:53:33 -0400

    [FIX] victorikus_web: Fix mosaic image view

--- victorikus_web/__manifest__.py
+++ victorikus_web/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'website', 'website_sale', 'website_sale_stock', 'account', 'stock', 'contacts'],
+    'depends': ['base', 'website', 'website_sale', 'website_sale_stock', 'account', 'stock', 'contacts', 'sale_management'],
 
     'installable': True,
 
