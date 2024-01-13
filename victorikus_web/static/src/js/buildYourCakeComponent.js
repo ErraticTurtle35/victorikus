06f3c9d09dfdc27483e6e14e2b91e94f592ea6ba
Gabriel Morales <gabs2323@outlook.com>
2024-01-13 19:27:22 -0400

    [ADD] Add basic owl component

--- victorikus_web/static/src/js/buildYourCakeComponent.js
+++ victorikus_web/static/src/js/buildYourCakeComponent.js
@@ -1,7 +1,7 @@
 odoo.define('victorikus_web.buildYourCake', function (require) {
     "use strict";
 
-    import {Component} from "@odoo/owl";
+    const {Component} = require('@odoo/owl');
 
     class BuildYourCakeComponent extends Component {
         static template = "victorikus_web.BuildYourCakeComponent";
